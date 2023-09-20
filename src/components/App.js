004a5063c0b9dbcaff9a4808dfb0d6b0e75112c5
Stanley Osagie Ramos <sagiecyber@gmail.com>
2023-09-20 17:13:17 +0100

    _

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import {
+  BrowserRouter, Route, Routes,
+} from 'react-router-dom';
 import Calculator from './Calculator';
 import QuoteComponent from './QuoteComponent';
 import NavLinks from './NavLinks';
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './components/App';
+import './index.scss';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(<App tab="home" />);
+root.render(<App />);

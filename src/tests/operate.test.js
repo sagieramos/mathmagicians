85d254b4d756950e6fd9415f1341a9d51a5f0833
Uzair Manzoor <uzair.kayani555@gmail.com>
2023-09-22 13:15:20 +0500

    add test for calculation and error handling

--- src/tests/operate.test.js
+++ src/tests/operate.test.js
@@ -18,3 +18,13 @@ test('divides two numbers correctly', () => {
   test('handles division by zero', () => {
     expect(operate(10, 0, '÷')).toBe("Can't divide by 0.");
   });
+
+  test('calculates the modulo correctly', () => {
+    expect(operate(10, 3, '%')).toBe('1');
+  });
+  test('handles modulo when dividing by zero', () => {
+    expect(operate(10, 0, '%')).toBe("Can't find modulo as can't divide by 0.");
+  });
+  test('throws an error for unknown operation', () => {
+    expect(() => operate(2, 3, '&')).toThrowError("Unknown operation '&'");
+  });

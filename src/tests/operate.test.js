11ee09e489bc06272c042ba7d4ecba88eadffe40
Uzair Manzoor <uzair.kayani555@gmail.com>
2023-09-22 13:25:08 +0500

    fix eslint errors

--- src/tests/operate.test.js
+++ src/tests/operate.test.js
@@ -9,22 +9,25 @@ test('subtracts two numbers correctly', () => {
 });
 
 test('multiplies two numbers correctly', () => {
-    expect(operate(4, 2, 'x')).toBe('8');
-  });
+  expect(operate(4, 2, 'x')).toBe('8');
+});
 
 test('divides two numbers correctly', () => {
-    expect(operate(8, 2, '÷')).toBe('4');
-  });
-  test('handles division by zero', () => {
-    expect(operate(10, 0, '÷')).toBe("Can't divide by 0.");
-  });
+  expect(operate(8, 2, '÷')).toBe('4');
+});
 
-  test('calculates the modulo correctly', () => {
-    expect(operate(10, 3, '%')).toBe('1');
-  });
-  test('handles modulo when dividing by zero', () => {
-    expect(operate(10, 0, '%')).toBe("Can't find modulo as can't divide by 0.");
-  });
-  test('throws an error for unknown operation', () => {
-    expect(() => operate(2, 3, '&')).toThrowError("Unknown operation '&'");
-  });
+test('handles division by zero', () => {
+  expect(operate(10, 0, '÷')).toBe("Can't divide by 0.");
+});
+
+test('calculates the modulo correctly', () => {
+  expect(operate(10, 3, '%')).toBe('1');
+});
+
+test('handles modulo when dividing by zero', () => {
+  expect(operate(10, 0, '%')).toBe("Can't find modulo as can't divide by 0.");
+});
+
+test('throws an error for unknown operation', () => {
+  expect(() => operate(2, 3, '&')).toThrowError("Unknown operation '&'");
+});
